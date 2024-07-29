f246b72ee140c97e6805945f2fa3e53d73c350a5
Chandan Veerabhadrappa <chandanv89@gmail.com>
2024-07-29 23:15:51 +0100

    Updated forge.config.js

--- forge.config.js
+++ forge.config.js
@@ -1,32 +1,45 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FusesPlugin } = require("@electron-forge/plugin-fuses");
+const { FuseV1Options, FuseVersion } = require("@electron/fuses");
+const path = require("path");
 
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: path.join(process.cwd(), "src/assets/base64.icon.png"),
   },
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: "@electron-forge/maker-squirrel",
+      config: { bin: "base64-codec" },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+      config: { bin: "base64-codec" },
     },
     {
-      name: '@electron-forge/maker-deb',
-      config: {},
+      name: "@electron-forge/maker-deb",
+      config: {
+        bin: "base64-codec",
+        options: {
+          icon: path.join(process.cwd(), "src/assets/base64.icon.png"),
+        },
+      },
     },
     {
-      name: '@electron-forge/maker-rpm',
-      config: {},
+      name: "@electron-forge/maker-rpm",
+      config: {
+        bin: "base64-codec",
+        options: {
+          icon: path.join(process.cwd(), "src/assets/base64.icon.png"),
+        },
+      },
     },
   ],
   plugins: [
     {
-      name: '@electron-forge/plugin-auto-unpack-natives',
+      name: "@electron-forge/plugin-auto-unpack-natives",
       config: {},
     },
     // Fuses are used to enable/disable various Electron functionality
@@ -40,5 +53,17 @@ module.exports = {
       [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
+  ],
+  publishers: [
+    {
+      name: "@electron-forge/publisher-github",
+      config: {
+        repository: {
+          owner: "chandanv89",
+          name: "base64-codec",
+        },
+        prerelease: true,
+      },
+    },
   ],
 };
